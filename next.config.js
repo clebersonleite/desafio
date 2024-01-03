cbd938e88b9b4f92c567caf15eefafc25a81f5f8
Cleberson Leite <kleberson1992@gmail.com>
2024-01-03 19:16:56 -0300

    fix: adjust next.config to githubpages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+
+    exportPathMap: function () {
+        return {
+            '/': { page: '/' },
+        };
+    },
 }
 
 module.exports = nextConfig
